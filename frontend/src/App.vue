1a434ee48faa6be6d3a4149cb8309a4254bcf561
DAVIDNGOLO <76950927+DAVIDNGOLO@users.noreply.github.com>
2022-06-27 12:38:26 +0100

    add frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Inscription /</router-link> 
+      <router-link to="/signin"> Connexion /</router-link>
+      <router-link to="/profil"> Profil</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+<script>
+
+</script>
+
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #4E5166;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #4E5166;
+}
+
+nav a.router-link-exact-active {
+  color: #FD2D01;
+}
+</style>
---
+++ frontend/src/views/ProfilView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="logoSignin">
+    <img alt="groupomania logo" src="../assets/icon-left-font.png" />
+    <div class="logoSignin">
+      <p class="acceuilSignin">
+        Bienvenue {{ nomUtilisateur}}
+      </p>
+    </div>
+    <div>
+      <h1>Profil {{ nomUtilisateur}}</h1>
+    </div>
+    <div class="signinConnexion">
+      <h2>Email</h2>
+      
+      <div>
+        <input type="email" >
+      </div>
+    </div>
+    <div class="signinConnexion">
+      <h2>Password</h2>
+      
+      <div>
+        <input type="password" >
+      </div>
+    </div>
+    <div>
+      <input class="Bouton"
+       type="button"
+       value="Déconnexion">
+    </div>
+    <div>
+    
+    
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
